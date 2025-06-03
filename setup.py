cd8cd89309858ab4dcc425d6c8736324e315968d
Vahid Saber <vahidsbr@amazon.de>
2025-06-03 18:29:11 +0200

    preparing for release v0.1.3

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="sql2data",
-    version="0.1.2",
+    version="0.1.3",
     author="Vahid Saber",
-    author_email="your.email@example.com",
+    author_email="vahid.saber78@gmail.com",
     description="Export SQL query results to Parquet, CSV, and more. Upload to S3 or MinIO.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -16,10 +16,10 @@ setup(
         "Bug Tracker": "https://github.com/vahid110/sql2data/issues",
     },
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Topic :: Database",
         "Topic :: Utilities",
@@ -35,6 +35,13 @@ setup(
         "python-dotenv",
         "tabulate"
     ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "coverage",
+            "duckdb"
+        ]
+    },
     entry_points={
         "console_scripts": ["sql2data=sql2data.cli.main:cli"],
     },
